36baa66505d0a3853698d64d3516f77d63bc17e5
Mauricio David Cuello Alzate <guasonito@Mauricios-MacBook-Pro.local>
2026-08-13 01:10:38 +0200

    fix: remove unused components and collections

--- src/types/index.ts
+++ src/types/index.ts
@@ -10,7 +10,6 @@ export type Contact = CollectionEntry<"contact">["data"];
 export type Homepage = CollectionEntry<"homepage">["data"];
 export type Features = CollectionEntry<"features">["data"];
 export type Project = CollectionEntry<"projects">["data"];
-export type Careers = CollectionEntry<"careers">["data"];
 export type Integrations = CollectionEntry<"integrations">["data"];
 
 // ─── Section Data Types ───────────────────────────────────────────────────────
